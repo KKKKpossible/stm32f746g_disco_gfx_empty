b9b2e44d0113cc67ea73f938c38344868bb14e3a
sjkim <ksj10111011@gmail.com>
2021-05-27 16:00:27 +0900

    gfx 프로젝트 추가

--- 32f746g_disco/Debug/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_dma_ex.d
+++ 32f746g_disco/Debug/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_dma_ex.d
@@ -1,80 +1,80 @@
 src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_dma_ex.o: \
  ../src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_dma_ex.c \
- D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal.h \
- D:/_0_stm32data/workspace/32f746g_disco/src/bsp/stm32f7xx_hal_conf.h \
- D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_rcc.h \
- D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_def.h \
- D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/CMSIS/Device/ST/STM32F7xx/Include/stm32f7xx.h \
- D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/CMSIS/Device/ST/STM32F7xx/Include/stm32f746xx.h \
- D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/CMSIS/Include/core_cm7.h \
- D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/CMSIS/Include/cmsis_version.h \
- D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/CMSIS/Include/cmsis_compiler.h \
- D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/CMSIS/Include/cmsis_gcc.h \
- D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/CMSIS/Include/mpu_armv7.h \
- D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/CMSIS/Device/ST/STM32F7xx/Include/system_stm32f7xx.h \
- D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
- D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_rcc_ex.h \
- D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_exti.h \
- D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_gpio.h \
- D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_gpio_ex.h \
- D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dma.h \
- D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dma_ex.h \
- D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_cortex.h \
- D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_flash.h \
- D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_flash_ex.h \
- D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_i2c.h \
- D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_i2c_ex.h \
- D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_pwr.h \
- D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_pwr_ex.h
+ D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal.h \
+ D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/bsp/stm32f7xx_hal_conf.h \
+ D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_rcc.h \
+ D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_def.h \
+ D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/CMSIS/Device/ST/STM32F7xx/Include/stm32f7xx.h \
+ D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/CMSIS/Device/ST/STM32F7xx/Include/stm32f746xx.h \
+ D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/CMSIS/Include/core_cm7.h \
+ D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/CMSIS/Include/cmsis_version.h \
+ D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/CMSIS/Include/cmsis_compiler.h \
+ D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/CMSIS/Include/cmsis_gcc.h \
+ D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/CMSIS/Include/mpu_armv7.h \
+ D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/CMSIS/Device/ST/STM32F7xx/Include/system_stm32f7xx.h \
+ D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_rcc_ex.h \
+ D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_exti.h \
+ D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_gpio.h \
+ D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_gpio_ex.h \
+ D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dma.h \
+ D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dma_ex.h \
+ D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_cortex.h \
+ D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_flash.h \
+ D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_flash_ex.h \
+ D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_i2c.h \
+ D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_i2c_ex.h \
+ D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_pwr.h \
+ D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_pwr_ex.h
 
-D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal.h:
+D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal.h:
 
-D:/_0_stm32data/workspace/32f746g_disco/src/bsp/stm32f7xx_hal_conf.h:
+D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/bsp/stm32f7xx_hal_conf.h:
 
-D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_rcc.h:
+D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_rcc.h:
 
-D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_def.h:
+D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_def.h:
 
-D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/CMSIS/Device/ST/STM32F7xx/Include/stm32f7xx.h:
+D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/CMSIS/Device/ST/STM32F7xx/Include/stm32f7xx.h:
 
-D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/CMSIS/Device/ST/STM32F7xx/Include/stm32f746xx.h:
+D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/CMSIS/Device/ST/STM32F7xx/Include/stm32f746xx.h:
 
-D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/CMSIS/Include/core_cm7.h:
+D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/CMSIS/Include/core_cm7.h:
 
-D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/CMSIS/Include/cmsis_version.h:
+D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/CMSIS/Include/cmsis_version.h:
 
-D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/CMSIS/Include/cmsis_compiler.h:
+D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/CMSIS/Include/cmsis_compiler.h:
 
-D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/CMSIS/Include/cmsis_gcc.h:
+D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/CMSIS/Include/cmsis_gcc.h:
 
-D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/CMSIS/Include/mpu_armv7.h:
+D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/CMSIS/Include/mpu_armv7.h:
 
-D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/CMSIS/Device/ST/STM32F7xx/Include/system_stm32f7xx.h:
+D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/CMSIS/Device/ST/STM32F7xx/Include/system_stm32f7xx.h:
 
-D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
 
-D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_rcc_ex.h:
+D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_rcc_ex.h:
 
-D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_exti.h:
+D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_exti.h:
 
-D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_gpio.h:
+D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_gpio.h:
 
-D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_gpio_ex.h:
+D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_gpio_ex.h:
 
-D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dma.h:
+D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dma.h:
 
-D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dma_ex.h:
+D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dma_ex.h:
 
-D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_cortex.h:
+D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_cortex.h:
 
-D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_flash.h:
+D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_flash.h:
 
-D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_flash_ex.h:
+D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_flash_ex.h:
 
-D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_i2c.h:
+D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_i2c.h:
 
-D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_i2c_ex.h:
+D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_i2c_ex.h:
 
-D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_pwr.h:
+D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_pwr.h:
 
-D:/_0_stm32data/workspace/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_pwr_ex.h:
+D:/_0_stm32data/0_repository/32f746g_gfx_empty/32f746g_disco/src/lib/32f746g_mx/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_pwr_ex.h:
